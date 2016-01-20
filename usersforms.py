fe9f28c89fca6fac462a90a3a8cfe3225600dc7c
kremerica <kremerica@gmail.com>
2016-01-19 23:32:58 -0800

    completed separation of users API and form

--- usersforms.py
+++ usersforms.py
@@ -1,15 +1,5 @@
-import users
 import webapp2
 
-class CreateUserWithParams(webapp2.RequestHandler):
-    def post(self):
-        phone=self.request.get("phone")
-        self.response.out.write('<li>' + phone + '</li>')
-
-        if users.createUser(phone):
-            self.response.write("success")
-        else:
-            self.response.write("user already exists")
 
 # create a new user with phone set to "phone" URL param
 class CreateUserForm(webapp2.RequestHandler):
@@ -17,11 +7,11 @@ class CreateUserForm(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
 
         self.response.write(
-            """<form method="post" action="/createuserwithparams">
+            """<form method="get" action="/createuser">
                 Enter phone number:<br>
                 <div><input type="text" name="phone"></div>
                 <div><input type="submit" value="Create Account"></div>
               </form>""")
 
 
-usersForms = [('/createuserwithparams', CreateUserWithParams), ('/createuserform', CreateUserForm)]
+usersForms = [('/createuserform', CreateUserForm)]
